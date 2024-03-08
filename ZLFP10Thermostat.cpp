a12331a2a8c5fee935754edfd682291bbee3b016
NickBKeenan <vendors@nickkeenan.com>
2024-03-08 17:00:56 -0500

    Revised to split into separate controller and thermostat
    The thermostat functionality is broken into a new library class, MultiStageThermostat, and the functions for interfacing with and controlling the FCU are broken into another library class, ZLFP10Controller.

--- ZLFP10Thermostat.cpp
+++ ZLFP10Thermostat.cpp
@@ -2,173 +2,35 @@
 
 
 #include "ZLFP10Thermostat.h"
+#include <ZLFP10Controller.h>
 
 ModbusMaster node;
 
-ZLFP10Thermostat::ZLFP10Thermostat(uint8_t pDHTSensorPin):MultiStageThermostat(pDHTSensorPin) //, DHT22,6)
-#ifdef DEBUGOUTPUTDEVICE_LCD
-,lcd(0x27, 20, 4)
-#endif
+ZLFP10Thermostat::ZLFP10Thermostat(uint8_t pDHTSensorPin):MultiStageThermostat(pDHTSensorPin) 
 {
-
+        Mode=MODE_OFF;
 }
 void ZLFP10Thermostat::setTempReader(uint8_t pTempReaderPin)
 {
-  TempReaderPin= pTempReaderPin;
+  FCUController.setTempReader(pTempReaderPin);
 }
 void ZLFP10Thermostat::setSerial(SoftwareSerial &pswSerial, uint8_t pRS485DEPin,uint8_t pRS485REPin)
 {
-    pswSerial.begin(9600);
-
-    node.begin(15, pswSerial);
-  DEBUGOUTLN("node begin");
-  node.SetPins(pRS485DEPin, pRS485REPin);
-  DEBUGOUTLN("Pins set");    
-
-
+  FCUController.setSerial(pswSerial,  pRS485DEPin,  pRS485REPin);
 }
 
 void ZLFP10Thermostat::setup() 
 {
-    DebugSetup();
-    DEBUGOUTLN("starting");
+   
+    DebugStream->println("starting");
 
 
     
   
-     pinMode(TempReaderPin, OUTPUT);
-     analogWrite(TempReaderPin, 134); // write a random value to silence the alarm
     setThermostatInterval(0.1);
     setDefaultStage(2);
     MultiStageThermostat::setup();
-    DEBUGOUTLN("done starting");
-}
-
-void ZLFP10Thermostat::ReadSettings() {
-  
-    
-    #define HOLDINGCOUNT  21
-    #define INPUTCOUNT  10
-    #define HOLDINGFIRST  0x6E8D
-    #define INPUTFIRST  0xB6D1
-
-    short holdingData[HOLDINGCOUNT];
-    short inputData[INPUTCOUNT];
-      uint8_t  result;
-    
-
-
-    result = node.readHoldingRegisters( HOLDINGFIRST, HOLDINGCOUNT);
-    if(result !=0)
-    {
-      DEBUGOUTLN("");
-      DEBUGOUT("Error reading holding registers ");
-      DEBUGOUT2(result, HEX);
-      DEBUGOUTLN("");
-      return;
-    }
-    int x;
-    for (x = 0; x < HOLDINGCOUNT; x++) {
-              holdingData[x] = node.getResponseBuffer(x);
-
-
-    }
-    delay(150);  // need a little bit of time between requests
-
-    result = node.readInputRegisters( INPUTFIRST, INPUTCOUNT);
-    if(result !=0)
-    {
-      DEBUGOUTLN("");
-      DEBUGOUT("Error reading input registers ");
-      DEBUGOUT2(result, HEX);
-      DEBUGOUTLN("");
-      
-      return;
-    }
-
-    for (x = 0; x < INPUTCOUNT; x++) {
-        inputData[x] = node.getResponseBuffer(x);
-    }
-
-    actualOnoff = holdingData[0];
-    if(holdingData[1]== FCU_MODE_AUTO) // auto temp mode, compare setpoints against actual temp
-    {
-      if(getTemp() > holdingData[11]/10) // cooling set point
-      {
-        Mode=MODE_COOL;
-      }
-      if(getTemp() < holdingData[12]/10) // cooling set point
-      {
-        Mode=MODE_HEAT;
-      }
-
-      // have to break this out because setpoint could change outside of mode change
-      if(Mode== MODE_HEAT)
-      {
-        FCUSetTemp=holdingData[12]/10;
-      }
-      if(Mode== MODE_COOL)
-      {
-        FCUSetTemp= holdingData[11]/10;
-      }
-
-    }
-    else
-    {
-      short FCUMode;
-      FCUMode = holdingData[1];
-      FCUSetTemp=0;
-      if(FCUMode==FCU_MODE_HEAT)
-      {
-        FCUSetTemp = holdingData[10]/10 ;
-        Mode=MODE_HEAT;
-      }
-      if(FCUMode==FCU_MODE_COOL)
-      {
-        FCUSetTemp = holdingData[9]/10 ;
-        Mode=MODE_COOL;
-      }
-  }
-  
-
-    reportedRoomTemp = inputData[0]/10;
-    coiltemp = inputData[1]/10;
-    if(reportedFanSetting != inputData[2] && !startingmode)
-    {
-      DEBUGOUTLN("");
-    }
-    
-    reportedFanSetting = inputData[2];
-    reportedRPM = inputData[3];
-    valveOpen = inputData[4];
-
-// fan setting is holdingData[2];
-
-    // fluttering detected, try to adjust setpoints
-    if(lastTempSetting !=0 && reportedRoomTemp!= lastTempSetting && millis()> nextAdjustmentTime && nextAdjustmentTime > 0)
-    {
-        DeFlutter();
-    }
-
-}
-
-
-void ZLFP10Thermostat::SetFanSpeed(int fanspeed)
-{
-  // version 3 -- look up setting in the table created in Calibrate()
-
-  int newSetting;
-  newSetting=SetLevels[fanspeed];
-  analogWrite( TempReaderPin, newSetting); 
-  delay(500); // to prevent read errors
-  DEBUGOUTLN("");
-  DEBUGOUT(" Setpoint=");
-  DEBUGOUT(newSetting);
-  DEBUGOUTLN("");
-  if(Mode==MODE_HEAT)
-    lastTempSetting=FCUSetTemp-fanspeed;
-  else
-    lastTempSetting=FCUSetTemp+fanspeed;
+    DebugStream->println("done starting");
 }
 
 
@@ -184,32 +46,32 @@ void ZLFP10Thermostat::DisplayStatus() {
     hour = minutes / 60;
     minutes -= hour * 60;
 
-    DEBUGOUT(hour);
-    DEBUGOUT(":");
+    DebugStream->print(hour);
+    DebugStream->print(":");
     if (minutes < 10)
-        DEBUGOUT('0');
-    DEBUGOUT(minutes);
-    DEBUGOUT(":");
+        DebugStream->print('0');
+    DebugStream->print(minutes);
+    DebugStream->print(":");
 
     if (seconds < 10)
-        DEBUGOUT('0');
-    DEBUGOUT(seconds);
+        DebugStream->print('0');
+    DebugStream->print(seconds);
     // sensor properties
-    DEBUGOUT(", Temp=");
-    DEBUGOUT2(getTemp(), 1);
+    DebugStream->print(", Temp=");
+    DebugStream->print(getTemp(), 1);
     
-    DEBUGOUT(", Humidity=");
-    DEBUGOUT2(getHumidity(), 0);
+    DebugStream->print(", Humidity=");
+    DebugStream->print(getHumidity(), 0);
     // thermostat properties
-    DEBUGOUT(", Setpoint:");
-    DEBUGOUT(FCUSetTemp );
-    DEBUGOUT(", Stage=");
-    DEBUGOUT(getLastStage());
-    DEBUGOUT(", U=");
-    DEBUGOUT2(upperthreshold,1);
-    DEBUGOUT(" L=");
-    DEBUGOUT2(lowerthreshold,1);
-    DEBUGOUT(" Next check=");
+    DebugStream->print(", Setpoint:");
+    DebugStream->print(FCUSetTemp );
+    DebugStream->print(", Stage=");
+    DebugStream->print(getLastStage());
+    DebugStream->print(", U=");
+    DebugStream->print(upperthreshold,1);
+    DebugStream->print(" L=");
+    DebugStream->print(lowerthreshold,1);
+    DebugStream->print(" Next check=");
     if (nextAdjustmentTime > millis())
     {
         Serial.print((nextAdjustmentTime-millis())/1000);
@@ -219,21 +81,23 @@ void ZLFP10Thermostat::DisplayStatus() {
 
 
     //FCU properties
-    DEBUGOUT(", Reported Room Temp=");
-    DEBUGOUT(reportedRoomTemp );
+    DebugStream->print(", Temp Pin=");
+    DebugStream->print(FCUController.lastTempPin);
+    DebugStream->print(", Reported Room Temp=");
+    DebugStream->print(FCUController.FCUSettings.RoomTemp );
     
     
-    DEBUGOUT(",  RPM=");
-    DEBUGOUT(reportedRPM);
+    DebugStream->print(",  RPM=");
+    DebugStream->print(FCUController.FCUSettings.FanRPM);
 
-    DEBUGOUT(", Fan setting=");
-    DEBUGOUT(reportedFanSetting);
-    DEBUGOUT(", Coil Temperature=");
-    DEBUGOUT(coiltemp);
-    DEBUGOUT(", Valve=");
-    DEBUGOUT(valveOpen);
-    DEBUGOUT("   " );
-    DEBUGOUT('\r');
+    DebugStream->print(", Fan setting=");
+    DebugStream->print(FCUController.FCUSettings.FanSetting);
+    DebugStream->print(", Coil Temperature=");
+    DebugStream->print(FCUController.FCUSettings.coilTemp);
+    DebugStream->print(", Valve=");
+    DebugStream->print(FCUController.FCUSettings.valveOpen);
+    DebugStream->print("   " );
+    DebugStream->print('\r');
 }
 int Delays[]=
 {
@@ -254,146 +118,10 @@ void ZLFP10Thermostat::RestartSession()
     setSettings(settings);
     setStageDelays(MAXFANSPEED+1, Delays);
 
-    Calibrate();
+    FCUController.Calibrate(Mode==MODE_HEAT, FCUSetTemp);
  
 }
 
-void ZLFP10Thermostat::Calibrate()
-{
-
-// the fan speed is setpoint minus reported temperature
-  // Using the onboard 5V, it seems that reported is roughly 165-input
-  
-  // So estimate the extremes of our range, 15C and 30C, measure what we get, and then interpolate the points in between
-  // Then test them against actual and adjust
-
-  // We only need three points 1,2,3. Zero is anything less than one, and four is anything less than three. 
-  //So figure out those three and then substract two from one to get zero, and add two to three to get four
-  DEBUGOUTLN("Calibrating...");
-  
-
-  short LevelL, LevelH;   // input levels for the extremes, low and high
-  short ReadingL, ReadingH; // room temperature readings we get back
-  LevelL= 165-15; // set for 15 C
-  LevelH=165-30;  // set for 30 C
-
-  //check out low end
-  analogWrite(TempReaderPin, LevelL);
-  delay(1000);
-  ReadSettings();
-  ReadingL=reportedRoomTemp;
-
-  // check out high end
-  analogWrite(TempReaderPin, LevelH);
-  delay(1000);
-  ReadSettings();
-  ReadingH=reportedRoomTemp;
-  DEBUGOUT(LevelL); //150
-  DEBUGOUT(" ");
-  DEBUGOUT(ReadingL); //170 17c
-  DEBUGOUT(" ");
-  DEBUGOUT(LevelH); //135
-  DEBUGOUT(" ");
-  DEBUGOUT(ReadingH); //300 30c
-  DEBUGOUT(" ");
-
-  // (LevelH-TargetLevel)/(LevelH-LevlL)= (ReadingH-TargetReading)/(ReadingH-ReadingL); or
-  // TargetLevel= LevelH - (LevelH-LevelL)*(ReadingH-TargetReading)/(ReadingH-ReadingL)
-// get initial guesses based on what the extremes of the range told us
-  short temps[3];
-  
-  if(Mode==MODE_HEAT)
-  {
-    temps[0]=(FCUSetTemp-1);
-    temps[1]=(FCUSetTemp-2);
-    temps[2]=(FCUSetTemp-3);
-  }
-  else
-  {
-    temps[0]=(FCUSetTemp+1);
-    temps[1]=(FCUSetTemp+2);
-    temps[2]=(FCUSetTemp+3);
-
-  }
-  SetLevels[1]=LevelH + (LevelL-LevelH)*(ReadingH-temps[0])/(ReadingH-ReadingL);
-  SetLevels[2]=LevelH + (LevelL-LevelH)*(ReadingH-temps[1])/(ReadingH-ReadingL);
-  SetLevels[3]=LevelH + (LevelL-LevelH)*(ReadingH-temps[2])/(ReadingH-ReadingL);
-
-// on the basement FCU, both 156 and 157 map to 20C. However, 156 flutters with 21C
-// This line forces it to pick 157, which seems stable
-// I'm not sure how to solve this, one idea would be to adaptively adjust the values when flutter happens.
- // SetLevels[2]=157;
-  int x;
-  
-  // go through the settings, set each one and then compare what happens and adjust
-  // Keep going until you can do it three times without adjustment, or 30 times total (failure)
- int goodTries=0; // number of sucessful reads   
-  for(x=0; x< 30 && goodTries < 3; x++)
-  {
-    int y;
-    DEBUGOUTLN("");
-    int goodLevels=0;    // number of successful reads in this iteration
-    for(y=1; y< 4; y++)
-    {
-      short reading;
-      DEBUGOUT(SetLevels[y]);
-      analogWrite(TempReaderPin, SetLevels[y]);
-      delay(1000);
-      ReadSettings();
-      reading=reportedRoomTemp;
-      DEBUGOUT(" ");
-      DEBUGOUT(reading);
-      DEBUGOUT(" . ");
-
-      if(reading != temps[y-1] )
-      {// need adjustment
-        DEBUGOUT(" * ");
-        SetLevels[y] += (reading-FCUSetTemp+y);
-        DEBUGOUT(SetLevels[y]);
-        DEBUGOUT(" * ");
-        
-      }
-      else
-      {// it works!
-        DEBUGOUT(" - ");
-        goodLevels++;
-      }
-      
-    }// go through each level
-    if(goodLevels==3) // every read on this try was successful
-    {
-      goodTries++;
-    }
-    else
-    {
-      goodTries=0; // reset to zero on failure, it has to be three in a row because failure also mean we adjust the values
-    }
-  
-  }// tries
-
-  if(Mode==MODE_HEAT)
-  {
-    SetLevels[0]=SetLevels[1]-2;
-    SetLevels[4]=SetLevels[3]+2;
-  }
-  else
-  {
-    SetLevels[0]=SetLevels[1]+2;
-    SetLevels[4]=SetLevels[3]-2;
-  }
-  if(goodTries==3)
-  {
-    DEBUGOUTLN("Done Calibrating");
-  }
-  else
-  {
-    DEBUGOUTLN("Unable to calibrate");
-    // trigger a P5 error
-    analogWrite(TempReaderPin,0);
-    while(1);
-  }
-  
-}
 
 void ZLFP10Thermostat::loop() 
 
@@ -412,14 +140,14 @@ void ZLFP10Thermostat::loop()
     if (oldtemp != temp || now > nextcheck  || nextcheck == 0)
     {
             short oldOnOff, oldMode, oldSetTemp;
-            oldOnOff=actualOnoff;
+            oldOnOff=Onoff;
             oldMode=Mode ;
             oldSetTemp= FCUSetTemp;
 
     
-            ReadSettings();
+            ReadFCUSettings();
             // if on/off, mode or setpoint has changed since last iteration, reset everything
-            if(oldOnOff!=actualOnoff || oldMode!=Mode ||  oldSetTemp!= FCUSetTemp)
+            if(oldOnOff!=Onoff || oldMode!=Mode ||  oldSetTemp!= FCUSetTemp)
             {
               RestartSession();
             }
@@ -428,7 +156,17 @@ void ZLFP10Thermostat::loop()
             int newStage=getStage();
             if(oldStage!=newStage || nextcheck==0)// first time in
             {
-              SetFanSpeed(newStage);
+              int targetTemp;
+              if(Mode==MODE_HEAT)
+              {
+                  targetTemp=FCUSetTemp-newStage;
+              }
+              else
+              {
+                  targetTemp=FCUSetTemp+newStage;
+              }
+              DebugStream->println();
+              FCUController.SetFanSpeed(newStage, targetTemp);
             }
 
             nextcheck = now +10000; // don't check for 60 seconds unless the temperature chnages
@@ -441,108 +179,59 @@ void ZLFP10Thermostat::loop()
 
 }
 
-void ZLFP10Thermostat::DeFlutter()
+void ZLFP10Thermostat::ReadFCUSettings()
 {
-  // we get here when we set the temperature, and then we read it back and it's not what we set it to.
-  // we have:
-  // settemp -- thermostat setting on the unit
-  // reportedRoomTemp  -- what the unit is reporting
-  // lastTempSetting -- what we set the temp to
-  DEBUGOUTLN();
-  DEBUGOUT("DEFluttering. Settemp=");
-  DEBUGOUT(lastTempSetting);
-  DEBUGOUT(" reported temp=");
-  DEBUGOUTLN(reportedRoomTemp);
-  DEBUGOUTLN();
+  FCUController.ReadSettings();  
 
-
-  if(Mode==MODE_HEAT)
-  { 
+  Onoff=FCUController.FCUSettings.Onoff;
     
-    if(lastTempSetting==FCUSetTemp)
+    if(FCUController.FCUSettings.Mode== FCU_MODE_AUTO) // auto temp mode, compare setpoints against actual temp
     {
-        if(reportedRoomTemp< lastTempSetting )
-        {
-          SetLevels[0]--;
-          DEBUGOUT("Adjusting level 0 to ");
-          DEBUGOUTLN(SetLevels[0]);
-          return;
-        }
-    }
-    if(lastTempSetting==FCUSetTemp-(MAXFANSPEED))
-    {
-        if(reportedRoomTemp> lastTempSetting )
-        {
-          SetLevels[MAXFANSPEED]++;
-          DEBUGOUT("Adjusting level ");
-          DEBUGOUT(MAXFANSPEED);
-          DEBUGOUT(" to ");
-          DEBUGOUTLN(SetLevels[MAXFANSPEED]);
-          return;
-        }
-    }
-    if(reportedRoomTemp > lastTempSetting)
-    {
-      // 
-      int setting =(FCUSetTemp-lastTempSetting);
-      DEBUGOUT(" Setting=");
-      DEBUGOUT(setting);
-      DEBUGOUT(" Level=");
-      DEBUGOUT(SetLevels[setting]);
-      DEBUGOUT(" Next Level=");
-      DEBUGOUT(SetLevels[setting+1]);
-
-      if(SetLevels[setting+1]-SetLevels[setting]>1)
+      
+      if(getTemp() > FCUController.FCUSettings.AutoCoolingSetpoint) // cooling set point
       {
-        SetLevels[setting]++;
-          DEBUGOUT("Adjusting level ");
-          DEBUGOUT(setting);
-          DEBUGOUT(" to ");
-          DEBUGOUTLN(SetLevels[setting]);
+        Mode=MODE_COOL;
       }
+      if(getTemp() < FCUController.FCUSettings.AutoHeatingSetpoint) // heating set point
+      {
+        Mode=MODE_HEAT;
+      }
+
+      // have to break this out because setpoint could change outside of mode change
+      if(Mode== MODE_HEAT)
+      {
+        FCUSetTemp=FCUController.FCUSettings.AutoHeatingSetpoint;
+      }
+      if(Mode== MODE_COOL)
+      {
+        FCUSetTemp= FCUController.FCUSettings.AutoHeatingSetpoint;
+      }
+
     }
     else
     {
-      
-      int setting =(FCUSetTemp-lastTempSetting);
-      if(SetLevels[setting]-SetLevels[setting-1]>1)
+      short FCUMode;
+      FCUMode = FCUController.FCUSettings.Mode;
+      FCUSetTemp=0;
+      if(FCUMode==FCU_MODE_HEAT)
       {
-        SetLevels[setting]--;
-          DEBUGOUT("Adjusting level ");
-          DEBUGOUT(setting);
-          DEBUGOUT(" to ");
-          DEBUGOUTLN(SetLevels[setting]);
+        FCUSetTemp = FCUController.FCUSettings.HeatSetpoint;
+        Mode=MODE_HEAT;
       }
+      if(FCUMode==FCU_MODE_COOL)
+      {
+        FCUSetTemp = FCUController.FCUSettings.CoolSetpoint;
+        Mode=MODE_COOL;
+      }
+  }
+  
 
-    }
-  } 
-  else
-  {
-    // cooling TBD
-  } 
 
 }
-void ZLFP10Thermostat::DebugSetup()
+
+
+void ZLFP10Thermostat::SetDebugOutput(Stream * pDebug)
 {
-  #ifdef DEBUGOUTPUT_ENABLED  
-    #ifdef DEBUGOUTPUTDEVICE_SERIAL
-      Serial.begin(9600);
-    #endif
-   #ifdef DEBUGOUTPUTDEVICE_LCD
-       lcd.init();  // initialize the lcd
-       lcd.clear();
-       // Print a message to the LCD.
-        lcd.backlight();
-        lcd.setCursor(0, 0);
-        lcd.print("--------------------");
-        lcd.setCursor(6, 1);
-        lcd.print("GEEEKPI");
-        lcd.setCursor(1, 2);
-        lcd.print("Arduino IIC Screen");
-        lcd.setCursor(0, 3);
-        lcd.print("--------------------");
-        lcd.clear();
-        lcd.setCursor(0, 0);
-   #endif
-   #endif
-}
+  DebugStream=pDebug;
+  FCUController.SetDebugOutput(pDebug);
+};
--- ZLFP10Thermostat.h
+++ ZLFP10Thermostat.h
@@ -1,47 +1,8 @@
-// define this if you want debug output
-#define DEBUGOUTPUT_ENABLED  
-// if you define DEBUGOUTPUT_ENABLED, you have to define one of the following two:
-//#define DEBUGOUTPUTDEVICE_LCD
-#define DEBUGOUTPUTDEVICE_SERIAL
 
-#ifdef DEBUGOUTPUT_ENABLED  
-  #ifdef DEBUGOUTPUTDEVICE_SERIAL
-    #define DEBUGOUT(x) Serial.print(x)
-    #define DEBUGOUT2(x,y) Serial.print(x,y)
-    #define DEBUGOUTLN(x) Serial.println(x)
-  #endif
-  #ifdef DEBUGOUTPUTDEVICE_LCD
-    #define DEBUGOUT(x) lcd.print(x)
-    #define DEBUGOUT2(x,y) lcd.print(x,y)
-    #define DEBUGOUTLN(x) lcd.setCursor(0, 0);lcd.print(x)
-  #endif
-#else
-  #define DEBUGOUT(x)
-  #define DEBUGOUT2(x,y)
-  #define DEBUGOUTLN(x) 
-#endif
 #include <MultiStageThermostat.h>
-#include <ModbusMaster.h>
-#include <SoftwareSerial.h>
-#ifdef DEBUGOUTPUTDEVICE_LCD
-  #include <LiquidCrystal_I2C.h>
-#endif  
-#include <DHT.h>
 
-// Set up a new SoftwareSerial object
-#define MAXFANSPEED 4
-#define MEDFANSPEED 3
-#define LOWFANSPEED 2
-#define ULOWFANSPEED 1
-#define MINFANSPEED 0  //  0=off
-#define THERMOSTAT_INTERVAL 2  // how many tenths of a degree above and below the setpoint to allow
-#define ADJUSTMENT_INTERVAL 180 // don't adust more than once every 3 minutes
-#define UNIT_ADDRESS 15 // the MODBUS address of the FCU. This can be changed through the control panel if needed.
-#define FCU_MODE_AUTO 0
-#define FCU_MODE_COOL 1
-#define FCU_MODE_DEHU 2
-#define FCU_MODE_VENT 3
-#define FCU_MODE_HEAT 4
+#include <ZLFP10Controller.h>
+
 
 
 
@@ -49,38 +10,29 @@ class ZLFP10Thermostat: public MultiStageThermostat
 {
 
   Settings settings;
+  ZLFP10Controller FCUController;
     unsigned long nextcheck = 0;   // track the last time we read settings and status from the unit, do it every minute
 
-    int TempReaderPin; // PWm pin that connects to FCU temp reader
+    
     
   
-    short actualOnoff;  // what the unit reports for onoff setting
+    short Onoff;  // what the unit reports for onoff setting
     short Mode;   // The mode we're in, heat or cool
-    short reportedFanSetting; // what the unit reports for fan speed setting
-    short reportedRoomTemp; // what the unit reports for room temp. Not used, just for curiosity
-    short reportedRPM;    // what the unit reports for fan rpm. Not used, just for curiosity
-    short coiltemp;     // what the unit reports for coil temperature Not used, just for curiosity
-    short valveOpen;  // what the unit reports for zone valve setting.Not used, just for curiosity 
-    short lastTempSetting=0; // used for deflutter
-    short FCUSetTemp;
-    short SetLevels[5]; // the digital output level for each fan speed, set in Calibrate(); 
-    #ifdef DEBUGOUTPUTDEVICE_LCD
-      LiquidCrystal_I2C lcd;
-    #endif
+    short FCUSetTemp; // the temperature setting the FCU is reporting
+    
+
+    Stream *DebugStream; // used for debug output, can be serial, LCD, or none
     void RestartSession(); // called when the mode or setpoint changes
+    void ReadFCUSettings(); // Get info from the FCUController
 public:
     ZLFP10Thermostat(uint8_t pDHTSensorPin);
-    
-    void setSerial(SoftwareSerial &pswSerial, uint8_t pRS485DEPin,uint8_t pRS485REPin);
-    void setTempReader(uint8_t pTempReaderPin);
+    void setSerial(SoftwareSerial &pswSerial, uint8_t pRS485DEPin,uint8_t pRS485REPin); // assign the softwareserial port used by MODBUS
+    void setTempReader(uint8_t pTempReaderPin); // assign the pin used for the FCU temp reader
     void setup();
-    void ReadSettings(); // read from onboard settings
-    void SetFanSpeed(int fanspeed); // write out to temperature spoof
-    void DisplayStatus();
+    void DisplayStatus();  // called once per loop
     void loop();
-    void Calibrate();   // calibrate temperature spoofing
-    void DeFlutter();  // if temperature fluttering is detected, revisit the calibration
-    void DebugSetup(); // setup the debug output device, can be Serial or LCD
+    
+    void SetDebugOutput(Stream * pDebug); // set the device for debug output
   
 
 };
